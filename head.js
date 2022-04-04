fe3d38fadbbe39bda535148b70026afbe9d2c6ca
Charles Ngo <charlesngo37@gmail.com>
2022-04-04 15:04:50 -0700

    Created function to obtain all items past first item in array. Linted both files

--- head.js
+++ head.js
@@ -13,7 +13,7 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   return array[0];
-}
+};
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(['Bagels', 'Donuts', 'Coffee']), 'Donuts');
