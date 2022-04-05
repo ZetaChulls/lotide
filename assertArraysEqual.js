aef22514ccbc25aa88e80c3bed2477b8e3ab6ac4
Charles Ngo <charlesngo37@gmail.com>
2022-04-05 16:04:00 -0700

    Implemented the array assertion using a previous helper function

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+// Helper function to compare two arrays.
+
+const eqArrays = function(array1, array2) {
+  // check if the arrays are the same length first
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // iterate through the array and compare the values at the same index. If any values dont match, return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if(eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+}; 
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
