74c54557f41a3db954393d0ede15eb604cef0125
Charles Ngo <charlesngo37@gmail.com>
2022-04-05 15:40:59 -0700

    code review and refactor performed. Removed break statement from for loop and used return instead

--- eqArrays.js
+++ eqArrays.js
@@ -11,21 +11,18 @@ const assertEqual = function(actual, expected) {
 // function will take in two arrays and compare both values at the same index and return true if they all match.
 
 const eqArrays = function(array1, array2) {
-// set initial output to false and check if the arrays are the same length first.
-  let output = false;
-  if (array1.length == array2.length) {
-// iterate through the array and compare the values at the same index. If any values dont match, return false;
-    for(let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]){
-        output = false;
-        break;
-      } else {
-        output = true;
-      }
+// check if the arrays are the same length first
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // iterate through the array and compare the values at the same index. If any values dont match, return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  return output;
-}
+  return true;
+};
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
