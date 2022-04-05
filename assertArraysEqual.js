07aea05ecbead2324a1bf88e77a692f14717b1bc
Charles Ngo <charlesngo37@gmail.com>
2022-04-05 16:46:41 -0700

    without functionality completed. linted with ESlint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -15,12 +15,12 @@ const eqArrays = function(array1, array2) {
 };
 
 const assertArraysEqual = function(actual, expected) {
-  if(eqArrays(actual, expected)) {
+  if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
   }
-}; 
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
