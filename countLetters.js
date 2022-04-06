c43c2b18d13304f5d75eebb1f67b7ba64c0df426
Charles Ngo <charlesngo37@gmail.com>
2022-04-06 16:14:10 -0700

    function that can count instances of a letter in a string

---
+++ countLetters.js
@@ -0,0 +1,42 @@
+// Helper function for assertion
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// A function that takes a sentence and returns a count for how often that letter appears in the sentence.
+
+// Function declaration 
+const countLetters = function (sentence) {
+  // Declare output variable.
+  const output = {};
+  // Remove all spaces from the strings
+  longSentence = sentence.replaceAll(' ', '');
+  // Loop through the long string.
+  for (let char of longSentence) {
+    // If the letter has appeared, increment the counter for that letter by 1
+    if (output[char]) {
+      output[char]++;
+    } else {
+      // If the letter has not appeared, create the key for the letter and set it to 1
+      output[char] = 1;
+    }
+  }
+  // Return an object which has the keys as the letters present and the value as how often they appeared.
+  return output;
+}
+
+  
+
+
+
+words = countLetters("hello");
+
+assertEqual(words.h, 1);
+assertEqual(words.e, 1);
+assertEqual(words.l, 2);
+assertEqual(words.o, 1);
