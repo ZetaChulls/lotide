77cecd75545296fb76fde9dde48a869d91f6049b
Charles Ngo <charlesngo37@gmail.com>
2022-04-06 16:55:52 -0700

    function to determine an object key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,45 @@
+// Helper function to determine assertion
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+
+};
+
+// Function that takes in an object and a value, and returns the first key that contains that value.
+
+
+// Function declaration. Takes 2 arguments, object and value
+const findKeyByValue = function(obj, item) {
+  // return the appropriate key. If no match, return undefined
+  let result;
+  let index = 0;
+  const objectKeys = Object.keys(obj)
+  
+  for (const value of Object.values(obj)) {
+    if (item === value) {
+      return objectKeys[index];
+    }
+    index++ 
+  }
+
+  return result;
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  action: "The Dark Knight",
+  youth: "Harry Potter"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Dark Knight"), "action");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Harry Potter"), "youth");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
