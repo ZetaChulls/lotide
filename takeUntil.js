59e0a97a9535d6e5d710ae34006a600558c42992
Charles Ngo <charlesngo37@gmail.com>
2022-04-07 14:59:22 -0700

    function implementation completed

---
+++ takeUntil.js
@@ -0,0 +1,29 @@
+// Function that will take an array and return every item until a condition is fulfilled.
+// Condition will be a callback
+
+const takeUntil = function(array, callback) {
+  let output = [];
+
+  // iterate through the entire array
+  for(const element of array) {
+    // continue to add elements to the output until the condition is fulfilled. 
+    if(!callback(element)){
+      output.push(element);
+    } else {
+      return output;
+    }
+  }
+  // breakout statement incase the condition is not fulfilled. 
+  return output;
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log("Expected output: [ 1, 2, 5, 7, 2 ], actual output: ", results1); 
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log("Expected output: [ 'I\'ve', 'been', 'to', 'Hollywood' ], Actual output: ", results2);
