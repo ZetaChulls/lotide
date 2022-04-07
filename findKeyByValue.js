5bed990ebaf9483eac610f809707471dbbfed2df
Charles Ngo <charlesngo37@gmail.com>
2022-04-06 17:52:33 -0700

    Refactored code to use for..in instead of for..of

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,20 +13,29 @@ const assertEqual = function(actual, expected) {
 
 
 // Function declaration. Takes 2 arguments, object and value
+// const findKeyByValue = function(obj, item) {
+//   // return the appropriate key. If no match, return undefined
+//   let result;
+//   let index = 0;
+//   const objectKeys = Object.keys(obj)
+//   for (const value of Object.values(obj)) {
+//     if (item === value) {
+//       return objectKeys[index];
+//     }
+//     index++ 
+//   }
+
+//   return result;
+// }
+
+// refactored above to use for..in and cycle through until a matching item is found.
 const findKeyByValue = function(obj, item) {
   // return the appropriate key. If no match, return undefined
-  let result;
-  let index = 0;
-  const objectKeys = Object.keys(obj)
-  
-  for (const value of Object.values(obj)) {
-    if (item === value) {
-      return objectKeys[index];
+  for (const key in obj) {
+    if (obj[key] === item) {
+      return key;
     }
-    index++ 
   }
-
-  return result;
 }
 
 
