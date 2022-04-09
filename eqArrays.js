fcfeb65740ba12248a7b57e0f055fb5a70cae6e2
Charles Ngo <charlesngo37@gmail.com>
2022-04-08 19:30:43 -0700

    Added recursion to eqArrays function to handle embedded arrays

--- eqArrays.js
+++ eqArrays.js
@@ -17,7 +17,9 @@ const eqArrays = function(array1, array2) {
   }
   // iterate through the array and compare the values at the same index. If any values dont match, return false;
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if(Array.isArray(array1[i]) || Array.isArray(array2[i])) {
+      eqArrays(array1[i], array2[i]);
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   }
@@ -30,3 +32,5 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
+assertEqual(eqArrays([["1", "2", "3"]], [["1", "2", "3"]]), true);
+assertEqual(eqArrays([[[[["1", "2", "3"]]], [1]]], [[[[["1", "2", "3"]]], [1]]]), true);
