851fa7c20935d922ea0d5a80d4c1be6e654dc99f
Charles Ngo <charlesngo37@gmail.com>
2022-04-08 19:18:23 -0700

    Added recusion to handle object cases

--- eqObjects.js
+++ eqObjects.js
@@ -37,13 +37,16 @@ const eqObjects = function(object1, object2) {
 
   // check the keys then the values to see if they have non-matching elements.
   for (let keys of object1Keys) {
+    if (typeof object1[keys] === 'object' && !Array.isArray(object1[keys]) || typeof object2[keys] === 'object' && !Array.isArray(object2[keys])){
+      eqObjects(object1[keys], object2[keys]);
+    }
     // Also check to see if either of the values are arrays. If so, run eqArray instead of checking the values directly
     if (Array.isArray(object1[keys]) || Array.isArray(object1[keys])) {
       if (!eqArrays(object1[keys], object2[keys])){
         return false;
       }
     // compare the values in each key to ensure it's the same.
-    } else if (!object2Keys.includes(keys) || !object2[keys].includes(object1[keys])) {
+    } else if (!object2Keys.includes(keys)) {
       return false;
     }
   }
@@ -63,3 +66,5 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: [1] }, b: [2] }, {  b: [2] , a: { z: [1] }}, ), true)
