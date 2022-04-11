eee55352fca8a49f88a1a4d0601116bffddb4c3a
Charles Ngo <charlesngo37@gmail.com>
2022-04-11 14:01:47 -0700

    Added mocha and chai for unit testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,22 @@
 // Importing function to test.
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// Test code to confirm head is working.
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Bagels', 'Donuts', 'Coffee']), 'Donuts');
-assertEqual(head(['Donuts']), 'Donuts');
-assertEqual(head([]), undefined);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 'Bagel' for ['Bagels', 'Donuts', 'Coffee']", () => {
+    assert.strictEqual(head(['Bagel']), 'Bagel');
+  });
+
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
